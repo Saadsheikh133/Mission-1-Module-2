fee5d06019dbf15a5cb4a01bb8e728cdd84c39a8
saadsheikh133 <mdsaadsheikh7096@gmail.com>
2025-11-04 23:17:21 +0600

    added Array.from method

--- M02V04.js
+++ M02V04.js
@@ -1,13 +1,23 @@
 // some
 
-const numbers = [1, 2, 3, 7, 5];
-const hasEvenNumber = numbers.some(number => number % 4 ===0);
-console.log(hasEvenNumber)
+// const numbers = [1, 2, 3, 7, 5];
+// const hasEvenNumber = numbers.some(number => number % 4 ===0);
+// console.log(hasEvenNumber)
 
 
-//*check user role using some
-const userAdminRole = ['user', 'creator','admin'];
-const featureAccessRole = ['admin', 'moderator'];
+// //*check user role using some
+// const userAdminRole = ['user', 'creator','admin'];
+// const featureAccessRole = ['admin', 'moderator'];
 
-const hasAdmin = userAdminRole.some(role => featureAccessRole.includes(role));
-console.log(hasAdmin);
+// const hasAdmin = userAdminRole.some(role => featureAccessRole.includes(role));
+// console.log(hasAdmin);
+
+
+// Array.from
+
+const range = (start, stop, step) => Array.from(
+    { length: Math.ceil((stop - start) / step) },
+    (_, i) => start + i * 2
+);
+
+console.log(range(1, 11, 2));
